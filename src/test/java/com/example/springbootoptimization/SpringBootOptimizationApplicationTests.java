bd7571df854af01b835d1e00209bd2c55e0863d6
Hazar Gul Nazari <hazar.nazari@favendo.com>
2022-04-03 12:43:32 +0200

    initial commit

---
+++ src/test/java/com/example/springbootoptimization/SpringBootOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootoptimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOptimizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
